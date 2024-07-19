d270fd1e84002659e1024d0704006a7b97161624
maede-ps <maaede.pasandideh@gmail.com>
2024-07-19 05:02:09 +0430

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# ethan-portfolio-starter
+# Portfolio Project
+
+a Portfolio website developed by next.js, tailwind css, swiper and framer motion
+
+## Installation
+
+Install admin-dashboard with npm
+
+```bash
+  npm install my-project
+  cd my-project
+```
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+## Running Tests
+
+To run tests, run the following command
+
+```bash
+  npm run test
+```
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  npm run deploy
+```
+
+## Features
+
+- Fully responsive
+- Designed by framer motion
+- Sliders developed by 'swiper'
+- Social media links
+- Testimonial page
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,30 @@
+import Link from "next/link";
+import { RiLinkedinBoxFill, RiInstagramLine, RiGithubFill, RiTelegramLine,RiTwitterXLine } from 'react-icons/ri';
+import { MdOutlineEmail } from "react-icons/md";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiLinkedinBoxFill />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiGithubFill />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiTelegramLine />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <MdOutlineEmail />
+      </Link>
+      <Link href="" className="hover:text-accent transition-all duration-300">
+        <RiTwitterXLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
